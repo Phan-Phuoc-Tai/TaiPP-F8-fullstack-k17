d3dde7db63ef50131461538de888af03f850c120
taipp <taip077201@gmail.com>
2026-01-14 16:45:18 +0700

    Bài tập về nhà buổi 36

--- day36/src/tools/httpRequest.jsx
+++ day36/src/tools/httpRequest.jsx
@@ -5,59 +5,57 @@ export const httpRequest = axios.create({
 });
 
 httpRequest.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem('access_token');
-  if (accessToken){
-    config.headers.Authorization = `Bearer ${accessToken}`
+  const accessToken = localStorage.getItem("access_token");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
   }
   return config;
 });
 
 let refreshPromise = null;
 const logout = () => {
-  localStorage.removeItem('access_token');
-  localStorage.removeItem('refresh_token');
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("refresh_token");
   return <Navigate to={"/login"} replace />;
-}
-const getNewToken = async () => {
-  try{
-    const refreshToken = localStorage.getItem('refresh_token');
-    const response = await fetch(`https://api.escuelajs.co/api/v1/auth/refresh-token`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        refreshToken,
-      })
-    })
+};
 
-    if (response.status !==201){
-      throw new Error('Unauthorized');
+const getNewToken = async () => {
+  try {
+    const refreshToken = localStorage.getItem("refresh_token");
+    const response = await httpRequest.post(`/refresh-token`, {
+      refreshToken,
+    });
+
+    if (response.status === 401) {
+      throw new Error("Unauthorized");
     }
-    return response.json()
-  } catch{
-    return false
+    return response.data;
+  } catch {
+    return false;
   }
-}
+};
 
 httpRequest.interceptors.response.use(
   (response) => {
     return response;
   },
-  async (err) => {
-    if (err.status === 401) {
+  async (error) => {
+    if (error.status === 401) {
       if (!refreshPromise) {
         refreshPromise = getNewToken();
       }
       const newToken = await refreshPromise;
-      if (newToken){
-        localStorage.setItem('access_token', newToken.access_token);
-        localStorage.setItem('refresh_token', newToken.refresh_token);
-        return httpRequest(err.config);
+      if (newToken) {
+        //Lưu vào localStorage
+        localStorage.setItem("access_token", newToken.access_token);
+        localStorage.setItem("refresh_token", newToken.refresh_token);
+        //Gọi lại request  bị failed
+        return httpRequest(error.config);
       } else {
+        //Logout
         logout();
-
       }
     }
+    return Promise.reject(error);
   }
 );
