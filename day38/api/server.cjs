78432e4651a914cdb93ecdf5fc5e03f9a8fcda06
taipp <taip077201@gmail.com>
2026-01-22 22:33:20 +0700

    Bài tập về nhà buổi 38

--- day38/api/server.cjs
+++ day38/api/server.cjs
@@ -3,17 +3,14 @@ const path = require("path");
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
-const dbPath = path.join(__dirname, "db.json");
-const router = jsonServer.router(dbPath);
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 
 server.use(middlewares);
-
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
   }),
 );
-
 server.use(router);
 
 module.exports = server;
